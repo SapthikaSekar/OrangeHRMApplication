0799911cf676effc1d72730bea8e93f971df84ba
balaji-githubstore <balaji0017@gmail.com>
2021-06-26 14:06:03 +0530

    Add project files.

---
+++ OrangeHrmApplication/AboutTest.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Maveric.OrangeHrmApplication
+{
+    class AboutTest
+    {
+        [Test]
+        public void AboutSectionTest()
+        {
+            //browserlaunch done
+            //login already done
+            //clickonprofileicon  //OrangePortalPage
+            //click on about  //OrangePortalPage
+            //Getthepoptext //OrangePortalPage
+        }
+    }
+}
---
+++ OrangeHrmApplication/OrangeHrmPages/LoginPage.cs
@@ -0,0 +1,37 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Maveric.OrangeHrmApplication.OrangeHrmPages
+{
+    class LoginPage
+    {
+        private static By usernameLocator = By.Id("txtUsername");
+        private static By passwordLocator = By.Id("txtPassword");
+        private static By loginLoctor = By.XPath("//*[@id='btnLogin']");
+        private static By errorLocator = By.Id("spanMessage");
+
+        public static void EnterUsername(IWebDriver driver,string username)
+        {
+            driver.FindElement(usernameLocator).SendKeys(username);
+        }
+
+        public static void EnterPassword(IWebDriver driver,string password)
+        {
+            driver.FindElement(passwordLocator).SendKeys(password);
+        }
+        public static void ClickOnLogin(IWebDriver driver)
+        {
+            driver.FindElement(loginLoctor).Click();
+        }
+
+        public static string GetErrorMessage(IWebDriver driver)
+        {
+            return driver.FindElement(errorLocator).Text;
+        }
+
+        //clickonforgotpassword
+        //geterrormessage
+    }
+}
