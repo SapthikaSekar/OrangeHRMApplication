344ac33a8e695a4bab9a1abb73057eb9f4b7f760
balaji-githubstore <balaji0017@gmail.com>
2021-06-26 14:02:20 +0530

    Add project files.

---
+++ OrangeHrmPages/DashboardPage.cs
@@ -0,0 +1,16 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Maveric.OrangeHrmApplication.OrangeHrmPages
+{
+    class DashboardPage
+    {
+        private static By employeeDistributionHeaderLocator = By.XPath("//legend[contains(text(),'Distribution')]");
+        public static string GetEmployeeDistributionByUnitHeader(IWebDriver driver)
+        {
+            return driver.FindElement(employeeDistributionHeaderLocator).Text;
+        }
+    }
+}
---
+++ OrangeHrmPages/OrangePortalPage.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OrangeHrmApplication.OrangeHrmPages
+{
+    class OrangePortalPage
+    {
+        //menu handle
+    }
+}
